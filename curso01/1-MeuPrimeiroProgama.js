89fa77c35aa9d002399d5279dc4fcaf2edd75aa6
Pedro Trudes <pedro.jtrudes@gmail.com>
2021-11-19 20:14:45 -0300

    usando const
    trabalhando com IF e ELSE para verificar a idade com JavaScript

---
+++ curso01/1-MeuPrimeiroProgama.js
@@ -0,0 +1,8 @@
+const nome = "pedro";
+const idade = 25;
+
+if (idade >= 19){
+    console.log("Ola "+ nome + " seja bem vindo!\nQuantos anos vc tem ? " + "Eu tenho "+ idade + "\nPode entrar!");
+}else {
+    console.log(nome + " Você não pode entrar");
+}
