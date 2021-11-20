c8365dcbe1d8bb291b9054c0851592a015e6c0d8
Pedro Trudes <pedro.jtrudes@gmail.com>
2021-11-20 18:27:26 -0300

    operações

--- curso01/1-MeuPrimeiroProgama.js
+++ curso01/1-MeuPrimeiroProgama.js
@@ -1,6 +1,7 @@
 const nome = "pedro";
 const idade = 25;
 
+
 if (idade >= 19){
     console.log("Ola "+ nome + " seja bem vindo!\nQuantos anos vc tem ? " + "Eu tenho "+ idade + "\nPode entrar!");
 }else {
---
+++ curso01/2-variaveis.js
@@ -0,0 +1,10 @@
+console.log("Trabalhando com variaveis");
+
+// const é o padrão corredo para criação de variaveis
+const idade = 21;
+
+// da para criar variaveis sem o const
+nome = "pedro";
+
+console.log(idade);
+console.log(nome);
