ca467be0c5b7b858637a3a76be50bdd54d53a3dc
Pedro Trudes <pedro.jtrudes@gmail.com>
2021-11-20 18:51:39 -0300

    conversão
    convertendo variáveis com LET

---
+++ curso01/4-conversao.js
@@ -0,0 +1,10 @@
+console.log("conversão de tipos")
+
+console.log("ano: " + 2020);
+console.log("2" + "2");
+
+console.log(parseInt("2") + parseInt("2"));
+console.log(parseFloat("2.5") + parseFloat("2.5"));
+
+console.log("10" / "2");
+console.log("pedro10" / "2"); // not a number
---
+++ curso01/5-atribuicao.js
@@ -0,0 +1,12 @@
+console.log("Trabalhando com atribuição");
+
+const idade = 21;
+const primeiroNome = "Pedro";
+const segundoNome = " Trudes";
+
+console.log(idade, primeiroNome , segundoNome);
+console.log(`meu nome é ${primeiroNome} ${segundoNome}`);
+
+const nomeCompleto = primeiroNome + segundoNome;
+console.log(`meu nome é : ${nomeCompleto}`);
+
