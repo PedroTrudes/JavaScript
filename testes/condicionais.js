cf49c5556b61cb45d93cb9700c55a1a59943e7ce
Pedro Trudes <pedro.jtrudes@gmail.com>
2021-11-24 00:50:50 -0300

    condicoes.js
    criando um arquivo de estudo com condições

---
+++ testes/condicionais.js
@@ -0,0 +1,11 @@
+console.log("Seja Bem Vindo!")
+
+const nome = "Pedro";
+const senha = 3005;
+
+if (nome == "Pedro" && senha == 3005) {
+    console.log("Logado com sucesso!");
+
+} else {
+    console.log("Senha ou login incorreto!\nTente novamente");
+}
