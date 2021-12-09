f119f28001a2d548a34cef4da01b710927d5a702
Pedro Trudes <pedro.jtrudes@gmail.com>
2021-12-09 16:47:34 -0300

    Retirando Arquivos
    Tirando arquivos que servia de teste

--- curso02/index.js
+++ curso02/index.js
@@ -12,15 +12,19 @@ cliente2.cpf = 55555555555;
 
 
 const contaCorrenteJoaquim = new ContaCorrente();
+
 contaCorrenteJoaquim.agencia = 1020;
 contaCorrenteJoaquim.cliente = cliente2;
 contaCorrenteJoaquim.depositar(500);
 
+
 const contaCorrentePedro = new ContaCorrente();
+
 contaCorrentePedro.agencia = 1001;
 contaCorrentePedro.cliente = cliente1;
 contaCorrentePedro.depositar(1000);
 contaCorrentePedro.sacar(500);
+
 
 contaCorrentePedro.trasferir(100, contaCorrenteJoaquim);
 
--- testes/01.js
+++
@@ -1,6 +0,0 @@
-onload = (event) => {
-
-    const texto= document.querySelector("#idTexto");
-    console.log('Texto inicial: ', texto.value);
-    
-    } ;
