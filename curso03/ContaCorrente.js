b9a0b05ee0105837f676f9fe48fac8f36358aa4b
Pedro Trudes <pedro.jtrudes@gmail.com>
2021-12-10 01:05:52 -0300

    Criando uma classe Conta
    Classe conta é onde todos as operações vão acontecer

--- curso03/ContaCorrente.js
+++ curso03/ContaCorrente.js
@@ -5,25 +5,11 @@ export class ContaCorrente {
     agencia;
     
     _cliente;
-    _saldo = 0;
 
-    set cliente(novoValor){
-      if(novoValor instanceof Cliente){
-      this._cliente = novoValor;
-    }
-    }
-
-    get cliente(){
-      return this._cliente;
-    }
-
-    get saldo(){
-      return this._saldo;
-    }
-    
     constructor (cliente, agencia) {
       this._cliente = cliente;
       this.agencia = agencia;
+      this._saldo = 0;
       ContaCorrente.numeroDeContas += 1;
     }
   
--- curso03/index.js
+++ curso03/index.js
@@ -1,20 +1,24 @@
 import { Cliente } from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
+import {Conta} from "./Conta.js";
 
 
 const cliente1 = new Cliente("Pedro", 45702873874);
-const cliente2 = new Cliente("Joaquim", 44444444444);
+const cliente2 = new Cliente("Joaquim0", 45222182124)
 
 
-const conta1 = new ContaCorrente(cliente1, 1001); 
+
+const conta1 = new Conta(0, cliente1, 1001); 
 conta1.depositar(1000);
 conta1.sacar(500);
 
-const conta2 = new ContaCorrente(cliente2, 102);
-conta2.depositar(5000);
+const conta2 = new Conta(cliente2, 1005);
 
 
-let valor = 200;
-conta1.trasferir(valor,conta2);
+const contaPoupanca1 = new Conta(50, cliente1, 1001)
 
-console.log(conta1,conta2,ContaCorrente.numeroDeContas);
+
+
+console.log(conta1);
+console.log(contaPoupanca1);
